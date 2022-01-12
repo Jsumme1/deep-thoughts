cf2acae936895ed685f5f9d719a9ac22478d39d9
Jsumme1 <88805050+Jsumme1@users.noreply.github.com>
2022-01-12 15:28:34 -0600

    mod 21.3 done

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 // import ApolloServer
+const path = require("path");
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require("./utils/auth");
 
@@ -33,6 +34,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
